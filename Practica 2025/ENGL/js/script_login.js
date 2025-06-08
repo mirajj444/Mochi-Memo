0280bedc192f978094c13f72d2f739f58d2fa5f7
mirajj444 <800crows@gmail.com>
2025-06-08 22:06:39 +0300

    Delete Practica 2025 directory

--- Practica 2025/ENGL/js/script_login.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("loginForm").addEventListener("submit", function (e) {
-  e.preventDefault();
-
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
-  const rememberMe = document.getElementById("remember").checked;
-
-  window.location.href = "index.html";
-});
